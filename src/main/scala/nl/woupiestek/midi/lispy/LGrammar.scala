996a25f2d6fe5dff32a2b952dd4c402241d36a09
Wouter Stekelenburg <woupiestek@yahoo.com>
2016-06-13 22:29:01 +0200

    Refactor grammar, add variables and comments

--- src/main/scala/nl/woupiestek/midi/lispy/LGrammar.scala
+++ src/main/scala/nl/woupiestek/midi/lispy/LGrammar.scala
@@ -6,74 +6,87 @@ object LGrammar {
 
   type G[T] = Grammar[Option[Char], T]
 
-  def track: G[Track] = {
+  class Context(entries: Map[String, Track]) {
+    def track: G[Track] = {
+      def args(name: String): G[Track] = name match {
+        case "note" => for {
+          key <- natural
+          duration <- natural
+        } yield Track(duration, List((0, NoteOn(0, key, 60)), (duration, NoteOff(0, key))))
+        case "rest" => number.map(d => Track(d, Nil))
+        case "seq" => track.zeroOrOne.map(_.foldLeft(Track.empty)(_ append _))
+        case "chord" => track.zeroOrOne.map(_.foldLeft(Track.empty)(_ stack _))
+        case "pui" => for {
+          x <- number
+          y <- track
+        } yield y piu x
+        case "cresc" => for {
+          x <- number
+          y <- track
+        } yield y cresc x
+        case "transpose" => for {
+          x <- number
+          y <- track
+        } yield y transpose x
+        case "channel" => for {
+          x <- natural
+          y <- track
+        } yield y toChannel x
+        case "put" => for {
+          x <- identifier
+          y <- track
+          z <- new Context(entries + (x -> y)).track
+        } yield z
+        case "get" => identifier.collect(entries)
+        case _ => Grammar.fail
+      }
 
-    def space: G[Unit] = Grammar.read[Option[Char]].collect {
-      case Some(c) if Set(' ', '\n', '\r', '\t', '\f').contains(c) => c
-    }.zeroOrMore.map(_ => ())
+      def separator: G[Unit] = for {
+        _ <- space
+        _ <- comment.zeroOrMore
+      } yield ()
 
-    def paren[T](g: G[T]): G[T] = for {
-      Some('(') <- Grammar.read[Option[Char]]
-      _ <- space
-      x <- g
-      Some(')') <- Grammar.read[Option[Char]]
-      _ <- space
-    } yield x
+      def space: G[Unit] = Grammar.read[Option[Char]].collect {
+        case Some(c) if Set(' ', '\n', '\r', '\t', '\f').contains(c) => c
+      }.zeroOrMore.map(_ => ())
 
-    def number: G[Int] = for {
-      sign <- Grammar.read[Option[Char]].collect { case Some('-') => () }.zeroOrOne
-      x <- Grammar.read[Option[Char]].collect { case Some(c) if ('0' to '9').contains(c) => c }.oneOrMore
-      _ <- space
-    } yield (if (sign.isEmpty) 1 else -1) * x.mkString.toInt
+      def comment: G[String] = {
+        def lineSep(x: Char): Boolean = x == '\n' || x == '\r'
+        for {
+          Some(';') <- Grammar.read[Option[Char]]
+          x <- Grammar.read[Option[Char]].collect {
+            case Some(x) if !lineSep(x) => x
+          }.zeroOrMore
+          Some(y) <- Grammar.read[Option[Char]] if lineSep(y)
+          _ <- space
+        } yield x.mkString
+      }
 
-    def small: G[String] = for {
-      x <- Grammar.read[Option[Char]].collect { case Some(c) if ('a' to 'z').contains(c) => c }.oneOrMore
-      _ <- space
-    } yield x.mkString
+      def natural: G[Int] = select(('0' to '9').contains).map(_.toInt)
 
-    def note: G[Track] = for {
-      "note" <- small
-      key <- number
-      duration <- number
-    } yield Track(duration, List((0, NoteOn(0, key, 60)), (duration, NoteOff(0, key))))
-    def rest: G[Track] = for {
-      "rest" <- small
-      key <- number
-      duration <- number
-    } yield Track(duration, Nil)
+      def select(f: Char => Boolean): G[String] = for {
+        x <- Grammar.read[Option[Char]].collect { case Some(c) if f(c) => c }.oneOrMore
+        _ <- separator
+      } yield x.mkString
 
-    def seq: G[Track] = for {
-      "seq" <- small
-      tracks <- track.zeroOrMore
-    } yield tracks.foldLeft(Track.empty)(_ append _)
-    def chord: G[Track] = for {
-      "chord" <- small
-      tracks <- track.zeroOrMore
-    } yield tracks.foldLeft(Track.empty)(_ stack _)
+      def number: G[Int] = natural | (for {
+        Some('-') <- Grammar.read[Option[Char]]
+        n <- natural
+      } yield -n)
 
-    def piu: G[Track] = for {
-      "piu" <- small
-      x <- number
-      y <- track
-    } yield y piu x
-    def cresc: G[Track] = for {
-      "cresc" <- small
-      x <- number
-      y <- track
-    } yield y cresc x
-    def transpose: G[Track] = for {
-      "transpose" <- small
-      x <- number
-      y <- track
-    } yield y transpose x
-    def channel: G[Track] = for {
-      "channel" <- small
-      x <- number
-      y <- track
-    } yield y toChannel x
+      def method: G[String] = select(('a' to 'z').contains)
 
-    paren(note | rest | seq | chord | piu | cresc | transpose | channel)
+      def identifier: G[String] = select(('<' to '~').contains)
+
+      for {
+        Some('(') <- Grammar.read[Option[Char]]
+        _ <- separator
+        name <- method
+        t <- args(name)
+        Some(')') <- Grammar.read[Option[Char]]
+        _ <- separator
+      } yield t
+    }
   }
-
 
 }
