d4c20ab22bf17ef62523be40db53d2346a4ef330
Wouter Stekelenburg <woupiestek@yahoo.com>
2016-07-02 23:51:34 +0200

    Refactor OtherSythesizerWrapper

--- src/main/scala/nl/woupiestek/midi/OtherSynthesizerWrapper.scala
+++ src/main/scala/nl/woupiestek/midi/OtherSynthesizerWrapper.scala
@@ -12,23 +12,22 @@ class OtherSynthesizerWrapper {
 
   def play(score: List[(Int, MidiMessage)]): Component[Unit] = {
     def await(time: Long): Unit = Some((time - System.nanoTime()) / million).filter(_ > 0).foreach(Thread.sleep)
-    synthesizer.map {
-      synthesizer =>
-        val channels = synthesizer.getChannels
-        synthesizer.open()
-        val ctm = System.nanoTime()
-        val s2 = score.sortBy[Int](_._1).map { case (t, e) => (t * million + ctm, e) }
-        for ((time, event) <- s2) {
-          await(time)
-          println(event)
-          event match {
-            case SetProgram(channel, program) => channels(channel).programChange(program)
-            case NoteOff(channel, pitch) => channels(channel).noteOff(pitch)
-            case NoteOn(channel, pitch, velocity) => channels(channel).noteOn(pitch, velocity)
-          }
+    def on(s2: List[(Long, MidiMessage)])(synthesizer: Synthesizer): Unit = {
+      val channels = synthesizer.getChannels
+      synthesizer.open()
+      for ((time, event) <- s2) {
+        await(time)
+        event match {
+          case SetProgram(channel, program) => channels(channel).programChange(program)
+          case NoteOff(channel, pitch) => channels(channel).noteOff(pitch)
+          case NoteOn(channel, pitch, velocity) => channels(channel).noteOn(pitch, velocity)
         }
-        synthesizer.close()
+      }
+      synthesizer.close()
     }
+    val ctm = System.nanoTime()
+    val s2 = score.sortBy[Int](_._1).map { case (t, e) => (t * million + ctm, e) }
+    synthesizer.map(on(s2))
   }
 
 }
