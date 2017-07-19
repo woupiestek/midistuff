f5a34ec54839bd96aa0bb9372f7ded5e55cd9f74
Wouter Stekelenburg <woupiestek@yahoo.com>
2017-07-19 22:31:25 +0200

    Correct track

--- src/main/scala/nl/woupiestek/midi/tagless/Track.scala
+++ src/main/scala/nl/woupiestek/midi/tagless/Track.scala
@@ -13,6 +13,10 @@ trait Track[M] {
   def setProgram(channel: Int, program: Int): M
 
   def setTempo(bpm: Int): M
+
+  def gained(m: M, velocity: Int): M
+
+  def transposed(m: M, d: Int): M
 }
 
 object TrackInstance extends Track[MidiMessage] {
@@ -29,6 +33,23 @@ object TrackInstance extends Track[MidiMessage] {
   override def setTempo(bpm: Int): MidiMessage = {
     val data = BigInt(60000000 / bpm).toByteArray
     new MetaMessage(META_TEMPO_TYPE, data, data.length)
+  }
+
+  override def gained(m: MidiMessage, gain: Int): MidiMessage = {
+    if ((m.getStatus & 0xF0) == NOTE_ON) {
+      val Array(status, key, velocity) = m.getMessage
+      new ShortMessage(NOTE_ON, status & 0x0F, key, velocity + gain)
+    } else m
+  }
+
+  override def transposed(m: MidiMessage, d: Int): MidiMessage = m.getStatus & 0xF0 match {
+    case NOTE_ON =>
+      val Array(status, key, velocity) = m.getMessage
+      new ShortMessage(NOTE_ON, status & 0x0F, key + d, velocity)
+    case NOTE_OFF =>
+      val Array(status, key) = m.getMessage
+      new ShortMessage(NOTE_OFF, status & 0x0F, key + d)
+    case _ => m
   }
 }
 
